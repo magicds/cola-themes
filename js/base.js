e1ad0d1b57c690a9e1cc46891aa7917cfca6a1e3
cdswyda <cdswyda@hotmail.com>
2017-04-02 13:09:18 +0800

    add search

---
+++ js/base.js
@@ -0,0 +1,18 @@
+// 搜索
+(function () {
+    var $searchInput = $('#search-input');
+    $searchInput.on('keyup',function (e) {
+        e.which === 13 && search();
+    });
+    
+    $('body').on('click','#search-icon',search);
+
+
+    var search = function () {
+        var key = $.trim($searchInput[0].value);
+        if (key.length === 0) return;
+
+        var loc = document.location;
+        document.location = loc.protocol + '//' + loc.host + '?s=' + key;
+    };
+})();
