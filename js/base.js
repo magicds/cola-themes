ca8da1503edf1edb05369cf793dcfb67b9dd505d
cdswyda <cdswyda@hotmail.com>
2017-07-14 21:38:59 +0800

    disqus

--- js/base.js
+++ js/base.js
@@ -52,10 +52,9 @@
      * 以promise的形式异步加载js，相同url仅加载一次。
      * 
      * @param {String} url 要加载的url
-     * @param {Boolean} noAdd 不要添加到页面 默认添加
      * @param {Boolean} noCache 不要缓存 默认会缓存 再次加载时直接返回加载好的promise
      */
-    CDSWYDA.loadJsPromise = function(url, noAdd, noCache) {
+    CDSWYDA.loadJsPromise = function(url, noCache) {
 
         if (this._jsPromise[url]) return this._jsPromise[url];
 
@@ -85,13 +84,9 @@
         }
         script.src = url;
         // append to head
-        !noAdd && document.getElementsByTagName('head')[0].appendChild(script);
+        document.body.appendChild(script);
 
-        if (noCache) {
-            return dtd.promise();
-        } else {
-            return (this._jsPromise[url] = dtd.promise());
-        }
+        return noCache ? dtd.promise() : (this._jsPromise[url] = dtd.promise());
     };
     /**
      * 异步加载js 回调函数方式
@@ -145,7 +140,7 @@
         console && console.log('测试能否使用Disqus');
         // 需要加个时间戳判断，防止缓存问题误以为可以加载Disqus
         // 加了时间戳 Disqus就不能使用了 不带时间戳则正常 所以需要加载两次吧 一次测试 一次正式
-        this.loadJsPromise(disqusCfg.jsUrl + '?_t=' + +new Date(), true, true).done(function() {
+        this.loadJsPromise(disqusCfg.jsUrl + '?_t=' + +new Date(), true).done(function() {
             disqusContainer.style.display = 'none';
             console && console.log('测试成功，Disqus可用，正在加载');
             // 可以使用 再次加载并初始化
@@ -158,46 +153,6 @@
                 bitComment = null;
             });
         }).fail(function() {
-            console && console.log('很抱歉您的网络不能使用Disqus!');
-            var bitcronTitle = el.querySelector('.comments_block_title');
-            if (!bitcronTitle) return;
-
-            var errTips = document.createElement('small');
-            errTips.className = 'net-error-tips';
-            errTips.innerText = '网络不通畅，为您显示Bitcron的评论框';
-            errTips.style.color = '#888';
-            errTips.style.marginLeft = '10px';
-            bitcronTitle.appendChild(errTips);
-        });
-    };
-    CDSWYDA.loadDisqus1 = function(el, disqusCfg) {
-        if (!disqusCfg) {
-            disqusCfg = {
-                domId: 'disqus_thread',
-                jsUrl: 'https://cdswyda.disqus.com/embed.js'
-            };
-        }
-        var disqusContainer = document.createElement('div');
-        disqusContainer.setAttribute('id', disqusCfg.domId);
-        disqusContainer.style.display = 'none';
-        el.appendChild(disqusContainer);
-
-        console && console.log('测试能否使用Disqus');
-        // 需要加个时间戳判断，防止缓存问题误以为可以加载Disqus
-        // 加了时间戳 Disqus就不能使用了 不带时间戳则正常 所以需要加载两次吧 一次测试 一次正式
-        this.loadJs(disqusCfg.jsUrl + '?_t=' + +new Date(), function() {
-            disqusContainer.style.display = 'none';
-            console && console.log('测试成功，Disqus可用，正在加载');
-            // 可以使用 再次加载并初始化
-            CDSWYDA.loadJs(disqusCfg.jsUrl, function() {
-                // 加载成功则显示Disqus，隐藏bitcron的
-                disqusContainer.style.display = 'block';
-
-                var bitComment = el.querySelector('.doc_comments');
-                bitComment && (bitComment.style.display = 'none');
-                bitComment = null;
-            });
-        }, function() {
             console && console.log('很抱歉您的网络不能使用Disqus!');
             var bitcronTitle = el.querySelector('.comments_block_title');
             if (!bitcronTitle) return;
